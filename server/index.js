d760c4a07f6f2f699d4be0d78a4da9816bb2f63a
УкрТехноСервис <uts.vlad@gmail.com>
2021-08-03 19:42:47 +0300

    Разделил проект на client и sever.  Добавил папку server и начал писать сервер и базы

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+const User = require('/models/user');
+
+const app = express();
+app.use(express.json());
