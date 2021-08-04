685cf1265b5fc4734528e7ee2dcd3aa0f08d53ff
УкрТехноСервис <uts.vlad@gmail.com>
2021-08-04 16:26:49 +0300

    дописан и протестирован сервер и работает подключенная БД

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config();
-
 const mongoose = require('mongoose');
+const BodyParser = require('body-parser');
+const Users = require('./models/users');
 
-const User = require('/models/user');
+const Posts = require('./models/posts');
+const Comments = require('./models/comments');
 
+dotenv.config();
+const port = process.env.APP_PORT;
 const app = express();
-app.use(express.json());
-app.use(BodyParser.json());
+// app.use(express.json());
+app.use(BodyParser());
+
+const {
+    followNewContact,
+    unfollowContact
+} = require('./route-handlers/user-handlers');
+
+
+
+const connectionString = process.env.CONNECTION_STRING;
+mongoose.connect(connectionString, {useNewUrlParser: true})
+    .then(
+        async()=>{
+        app.listen(port, ()=>{
+            console.log(`App is listening on port: ${port}`)
+        })
+    });
