aa554de74c79ab3f465f27a3e2c6e160a4dc3a15
УкрТехноСервис <uts.vlad@gmail.com>
2021-08-17 21:18:26 +0300

    ПРАВИЛЬНО переделана стилизация страницы MainPage

--- client/src/components/avatarName.jsx
+++ client/src/components/avatarName.jsx
@@ -2,15 +2,43 @@ import React from 'react';
 import styled from "styled-components";
 import Avatar from '@material-ui/core/avatar';
 import Box from '@material-ui/core/box';
+import {makeStyles} from '@material-ui/core/styles';
 
-const AvatarName = ({nick, loggedInUser = true, large = false, src, handler  }) => {
+const AvatarName = ({nick, loggedInUser = true, large = false, src, handler}) => {
+// !!! кастомная стилизация material-ui компонентов делается ТОЛЬКО с помощью makeStyles()
+    const useStyles = makeStyles({
+        Box_style: {   //доп стилизация material-ui:  https://material-ui.com/ru/styles/basics/
+            display: 'flex',
+            maxWidth: "18em",
+            minWidth: '8em',
+            flexDirection: 'row',
+            backgroundColor: 'lightgoldenrodyellow',
+            borderRadius: '4px',
+            border: '1px solid lightgray',
+
+            "&:hover": {
+                cursor: 'pointer'
+            }
+        },
+        BoxStyledLoggedIn: {
+            display: 'flex',
+            maxWidth: "18em",
+            minWidth: '10em',
+            flexDirection: 'row',
+            borderRadius: '4px',
+            border: '1px solid lightgray',
+            backgroundColor: 'lightcoral',
+        }
+    });
+    const classes = useStyles();
 
     return (
-        <Box className='user-avatar'  onClick={handler }
-             style={loggedInUser ? BoxStyledLoggedIn : Box_style}
+        <Box
+            className={loggedInUser ? classes.BoxStyledLoggedIn : classes.Box_style}
+            onClick={handler}
         >
-            {!large && <AvatarStyled alt="user-avatar" src={src}/> }
-            {large && <AvatarLargeStyled alt="user-avatar" src={src}/> }
+            {!large && <AvatarStyled alt="user-avatar" src={src}/>}
+            {large && <AvatarLargeStyled alt="user-avatar" src={src}/>}
             <P>{nick}</P>
         </Box>
     );
@@ -37,26 +65,4 @@ margin: 0 auto;
 font-size: 12px;
 line-height: 25px;
 `;
-const Box_style = {
-    display: 'flex',
-    maxWidth: "18em",
-    minWidth: '8em',
-    flexDirection: 'row',
-    backgroundColor: 'lightgoldenrodyellow',
-    borderRadius: '4px',
-    border: '1px solid lightgray',
-
-    "&:hover": {
-        cursor: 'pointer'
-    }
-};
-const BoxStyledLoggedIn = {
-    display: 'flex',
-    maxWidth: "18em",
-    minWidth: '10em',
-    flexDirection: 'row',
-    borderRadius: '4px',
-    border: '1px solid lightgray',
-    backgroundColor: 'lightcoral',
-};
 
