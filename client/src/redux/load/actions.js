155cfb1359992f7763d1ad23659c9bc9ee0fb422
УкрТехноСервис <uts.vlad@gmail.com>
2021-09-10 00:41:04 +0300

    refactured fetch().then()  на  async - await структуру и перепискал выводы сообщений

--- client/src/redux/load/actions.js
+++ client/src/redux/load/actions.js
@@ -1,47 +1,49 @@
 import {types} from './';
 
-export const loadUserPosts = (url) => dispatch => {
+export const loadUserPosts = (url) => async dispatch => {
     dispatch(postsLoading(true));
-    fetch(url, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then((res) => {
-            if (!res.ok) {
-                throw new Error()
-            } else {
-                return res.json()
+    try {
+        const data = await fetch(url, {
+            headers: {
+                "Content-Type": "application/json"
             }
-        }
-    ).then((data) => {
+        }).then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                } else {
+                    return res.json()
+                }
+            }
+        );
         dispatch({
             type: types.LOAD_USER_POSTS,
             payload: data
         });
-        dispatch(postsLoading(false));
-    })
-        .catch((err) => {
-            console.warn("на этапе получения постов пользователя возникла ошибка. Проверьте соединение");
-        });
+    } catch {
+        console.warn("на этапе получения постов пользователя возникла ошибка. Проверьте соединение")
+    }
 
+    dispatch(postsLoading(false));
 };
 
-export const loadOneUser = (url) => dispatch => {
+export const loadOneUser = (url) => async dispatch => {
     dispatch(userLoading(true));
+    try {
+        const currentUser = await fetch(url, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(r => r.json());
 
-    fetch(url, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(r => r.json())
-        .then(currentUser => {
-            dispatch({
-                type: types.LOAD_CURRENT_USER,
-                payload: currentUser
-            });
-            dispatch(userLoading(false));
-        })
-        .catch((err) => console.error("Ошибка загрузки пользователя."));
+        dispatch({
+            type: types.LOAD_CURRENT_USER,
+            payload: currentUser
+        });
+        dispatch(userLoading(false));
+
+    } catch {
+        console.error("Ошибка загрузки пользователя.")
+    }
 };
 
 export const postsLoading = (yesNo) => ({
@@ -54,68 +56,73 @@ export const userLoading = (yesNo) => ({
     payload: yesNo
 });
 
-export const toggleContactStatus = (contactNick, activeUserId) => dispatch => {
+export const toggleContactStatus = (contactNick, activeUserId) => async dispatch => {
     const url = `/users/`;
-    fetch(url, {
-        method: "PUT",
-        body: JSON.stringify({
-            contactNick,
-            activeUserId
-        }),
-        headers: {"Content-Type": "application/json"}
-    })
-        .then(r => r.json())
-        .then(res => {
-            dispatch({
-                type: types.TOGGLE_CONTACT_STATUS,
-                payload: res
 
-            });
+    try {
+        const res = await fetch(url, {
+            method: "PUT",
+            body: JSON.stringify({
+                contactNick,
+                activeUserId
+            }),
+            headers: {"Content-Type": "application/json"}
         })
-        .catch((err) => console.error("Что-то пошло не так при смене статуса пользователя"));
+            .then(r => r.json());
+
+        dispatch({
+            type: types.TOGGLE_CONTACT_STATUS,
+            payload: res
+        });
+    } catch {
+        console.error("Что-то пошло не так при смене статуса пользователя")
+    }
 
 
 };
 
-export const getPostAndComments = (pictureSrc) => dispatch => {
+export const getPostAndComments = (pictureSrc) => async dispatch => {
     const url = `/post/`;
+    try {
+        const res = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({pictureSrc}),
+            headers: {"Content-Type": "application/json"}
+        }).then(r => r.json());
 
-    fetch(url, {
-        method: "POST",
-        body: JSON.stringify({pictureSrc}),
-        headers: {"Content-Type": "application/json"}
-
-    }).then(r => r.json()).then(res => {
         dispatch({
             type: types.GET_POST_COMMENTS_USER,
             payload: res
         });
-        dispatch({type: types.OPEN_MODAL, payload: true});
-    }).catch(err => console.error(err.message));
+    } catch {
+        console.error("ошибка получения постов данного пользователя - проверьте подключение к интернет")
+    }
+    dispatch({type: types.OPEN_MODAL, payload: true});
 };
 
-export const updateLikeStatus = (postId, activeUserId) => dispatch => {
+export const updateLikeStatus = (postId, activeUserId) => async dispatch => {
 
     dispatch({type: types.CLEAR_ALL_USERS_POSTS});
+    try {
+        const res = await fetch('/post/likes', {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                postId, activeUserId
+            })
+        }).then(r => r.json());
 
-    fetch('/post/likes', {
-        method: "POST",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            postId, activeUserId
-        })
-    }).then(r => r.json())
-        .then(res => {
-                dispatch({
-                    type: types.UPDATE_LIKE_STATUS,
-                    payload: res
-                });
-            }
-        ).catch(err => console.warn("Сбой данный при получении данных при обновлении статуса лайка поста"));
+        dispatch({
+            type: types.UPDATE_LIKE_STATUS,
+            payload: res
+        });
+    } catch {
+        console.warn("Сбой данный при получении данных при обновлении статуса лайка поста")
+    }
 };
 
- const fetchPosts = (lastDate, postsPerBatch, activeUserId) =>dispatch=> {
-    fetch(`/posts/latest/`, {
+const fetchPosts = (lastDate, postsPerBatch, activeUserId) => async dispatch => {
+    const data = await fetch(`/posts/latest/`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
@@ -125,14 +132,10 @@ export const updateLikeStatus = (postId, activeUserId) => dispatch => {
             limit: postsPerBatch,
             activeUserId: activeUserId
         })
+    }).then(r => r.json());
 
-    }).then(r => r.json())
-        .then(data => {
-            dispatch({type: types.GET_ALL_USERS_POSTS, payload: data});
-        });
+    dispatch({type: types.GET_ALL_USERS_POSTS, payload: data});
 };
-
-
 
 
 export default {
