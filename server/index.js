74ac32c94d1bdaa36effbc38cfc5a39a351e3f2b
УкрТехноСервис <uts.vlad@gmail.com>
2021-09-16 14:05:32 +0300

    done

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose');
 const BodyParser = require('body-parser');
 
 dotenv.config();
-const port = process.env.APP_PORT;
+// const port = process.env.APP_PORT;  //так пропишем сами порт из файла .env
+const port = process.env.PORT || 8000;   //а так получим порт от heroku через Node environment
+
 const app = express();
 
-app.use(BodyParser.urlencoded({extended: true}));
-app.use(BodyParser.json());   // это вместо:  app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
+app.use(BodyParser.urlencoded({extended: true}));
+
+app.use(BodyParser.json());   // это вместо:  app.use(express.json());
 
 const {
     getUserByUserNick,
@@ -39,7 +43,6 @@ app.post('/newcomments/', createManyNewComments);
 // retreivers:
 app.get('/posts/:userNick', userPostsPage); //получить все посты указанного юзера
 app.post('/posts/latest/', latestPostsFeed);
-// app.get('/posts/latest/:lastDate/:limit/:activeUserId', latestPostsFeed);
 app.post('/post/', onePostModalPage);
 app.get('/users/:userNick', getUserByUserNick); //получить объект юзера
 app.get('/users_/:userId', getUserById);
@@ -47,6 +50,10 @@ app.post('/users/', getuserLists);
 app.put('/users', followUnfullowHandler);
 app.post('/comments/', postComment);
 app.post('/post/likes', likeUnlikeComment);
+
+app.get('*', (req, res)=> {
+    res.sendFile(__dirname + 'public/index.html');
+});
 
 const connectionString = process.env.CONNECTION_STRING;
 mongoose.connect(connectionString, {useNewUrlParser: true})
